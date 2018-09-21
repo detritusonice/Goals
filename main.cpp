aa406904fec0bae59d796b5d60bed1e399765601
Thanasis Karpetis <tkarpetis@gmail.com>
2018-09-21 21:38:58 +0300

    goals now reads an existing goals.xml file. Update iteration A todo.txt

--- main.cpp
+++ main.cpp
@@ -11,13 +11,24 @@
 
 int main( int argc, char** argv) {
 	// initialize program
+	GoalContainer gc;
 	// open files
 	// read goals
+	try {
+		gc.loadFile("goals.xml");
+	} catch (std::exception &e) {
+		std::cerr<<"Exception caught while reading XML file:"<<e.what()<<"\n\n";
+		return 1;
+	}
+	bool done=false;
 	// state machine loop
-	// display
-	// input
+	while ( !done ) {
+		// display
+		gc.printAll(std::cout);// dump all entries to standard output
+		// input
+		done=true;
 	// action
-	// end of loop
+	} // end of loop
 	// save or not (according to user instructions)
 	return 0;
 }
